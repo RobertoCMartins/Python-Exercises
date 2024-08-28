74df28b72b8a59b9531979c2bb48ced28a4702a5
Roberto C. Martins <atacadoblu@hotmail.com>
2024-08-28 16:14:50 -0300

    Adicionando funções matemáticas

---
+++ 1.Estrutura_SEQUENCIAL/Funcoes_Matematicas_Integradas.py
@@ -0,0 +1,17 @@
+# Encontrar número máximo e número mínimo
+x = (1, 15, 31)
+numeroMaximo = max(x)
+print(f'Valor do maior número de {x} é = {numeroMaximo}')
+numeroMinimo = min(x)
+print(f'Valor do menor número de {x} é = {numeroMinimo}')
+
+# Retorna valor absoluto (positivo) do número
+y = -520
+valorPositivo = abs(y)
+print(f'O valor positivo do numero {y} é = {valorPositivo}')
+
+# Calcula valor de um número elevado potencia de outro
+a = 5
+b = 3
+elevadoPotencia = pow(a,b)
+print(f'Valor de {a} elevado a potência de {b} é = {elevadoPotencia}')
---
+++ 1.Estrutura_SEQUENCIAL/Funcoes_Matematicas_modulo_Import_math.py
@@ -0,0 +1,21 @@
+import math
+
+# Método que calcula Raiz Quadrada
+a = float(input("Digite um número: "))
+raizQuadrada = math.sqrt(a)
+print(f'A raiz quadrada de {a:.2f} é = {raizQuadrada:.2f}')
+
+
+# Método que arredonda o número para cima
+b = 10.5
+arredondaCima = math.ceil(b)
+print(f'Número {b} arredondado para cima é = {arredondaCima}')
+
+# Método que arredonda o número para baixo
+c = 10.5
+arredondaCima = math.floor(c)
+print(f'Número {c} arredondado para cima é = {arredondaCima}')
+
+# Constante que retorna o número de PI
+numeroPI = math.pi
+print(f'Valor do PI é = {numeroPI}')
